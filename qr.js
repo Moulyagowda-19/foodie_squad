25024cbf0db81883b3976ba440cb269b2973625b
Naveen4421 <navi25231@gmail.com.com>
2025-11-23 16:15:29 +0530

    Initial project setup

---
+++ qr.js
@@ -0,0 +1,8 @@
+const QRCode = require('qrcode');
+
+const websiteURL = "https://foodiesquad.com"; // ✅ Replace with your real website link
+
+QRCode.toFile('foodie-qr.png', websiteURL, function (err) {
+  if (err) throw err;
+  console.log("✅ QR Code Generated! Check foodie-qr.png");
+});
